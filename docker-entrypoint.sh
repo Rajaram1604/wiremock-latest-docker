c49c977daf08d4855e70e606f04861a798a04451
Rajaram1604 <57511442+Rajaram1604@users.noreply.github.com>
2019-11-12 16:56:12 +0000

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 # Add `java -jar /wiremock-standalone.jar` as command if needed
-if [ "${1:0:1}" = "-" ]; then
+if [ "${1#-}" != "$1" ]; then
 	set -- java $JAVA_OPTS -cp /var/wiremock/lib/*:/var/wiremock/extensions/* com.github.tomakehurst.wiremock.standalone.WireMockServerRunner "$@"
 fi
 
@@ -12,7 +12,7 @@ if [ "$uid" != "" ]; then
 	# Change the ownership of /home/wiremock to $uid
 	chown -R $uid:$uid /home/wiremock
 
-	set -- gosu $uid:$uid "$@"
+	set -- su-exec $uid:$uid "$@"
 fi
 
 exec "$@"
